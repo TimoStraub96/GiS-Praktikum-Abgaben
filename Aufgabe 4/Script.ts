cb255742be94b297bae4fd07ce67ba1ed2d9655f
TimoStraub96 <timo.straub@hs-furtwangen.de>
2021-11-24 13:42:55 +0100

    lost

--- Aufgabe 4/Script.ts
+++ Aufgabe 4/Script.ts
@@ -1,39 +1,57 @@
 namespace testNameSpace {
-    const inputInterpret: HTMLInputElement = <HTMLInputElement>document.getElementById("input.interpret");
-    const inputPrice: HTMLInputElement = <HTMLInputElement>document.getElementById("input-price");
-    const display: HTMLInputElement = <HTMLInputElement>document.querySelector("#display");
-    const Eintragen: HTMLButtonElement = <HTMLButtonElement>document.querySelector("Eintragen");
-    const Laden: HTMLButtonElement = <HTMLButtonElement>document.querySelector("Laden");
-    const Löschen: HTMLButtonElement = <HTMLButtonElement>document.querySelector("Löschen");
+    const inputInterpret: HTMLInputElement = <HTMLInputElement>document.getElementById("inputInterpret");
+    const inputPrice: HTMLInputElement = <HTMLInputElement>document.getElementById("inputPrice");
+    const display: HTMLElement = <HTMLElement>document.querySelector("#display");
+    const myButton: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#machEtwas")
 
-    const tr = document.createElement("tr");
+
+    myButton.addEventListener("click", ():void=> {
+        createTr();
+    } );
+    
+    function createTr() {
+    const tr =document.createElement("tr");
     const td = document.createElement("td");
 
     td.textContent = inputInterpret.value;
     td.textContent = inputPrice.value;
 
-    const editBtn = document.createElement("button");
-    editBtn.textContent = "remove";
-
-    LadeButton();
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = "remove";
 
     tr.appendChild(td);
-    tr.appendChild(editBtn);
-    tr.appendChild(Eintragen);
-    tr.appendChild(Laden);
-
-
-
-    Eintragen.addEventListener("click", EintragenButton)
-
-    function EintragenButton() {
-        let aktuellerWert; String = inputInterpret.value;
-        localStorage.setItem("InputWert", aktuellerWert);
-    }
-    function LadeButton(){
-        let wertLocalStorage: string = localStorage.getItem("aktuellerWert");
-        inputInterpret.value= wertLocalStorage;
+    tr.appendChild(removeBtn);
     }
 
+    
+    let arrayFromStorageAsString: string = localStorage.getItem("localStorageElement");
+    let numbersArray: number[] = JSON.parse(arrayFromStorageAsString);
+    console.log(numbersArray);
 
+    myButton.addEventListener("click", mybuttonHandler);
+    console.log(inputInterpret);
+    console.log(inputPrice);
+    
+
+    let array: number[] = [12, 15, 17, 20];
+    let arrayString: string = JSON.stringify(array);
+
+    localStorage.setItem("localStorageElement", arrayString);
+
+    function mybuttonHandler() {
+        let interpretValue = inputInterpret.value;
+        let priceValue: number = Number(inputPrice.value);
+        
+        let arrayFromStorageAsString: string = localStorage.getItem("localStorageElement");
+        let numbersArray: number[] = JSON.parse(arrayFromStorageAsString);
+        console.log(numbersArray);
+
+        console.log(numbersArray[0] * numbersArray[2]);
+
+      
+        let newElement = document.createElement("div");
+        newElement.textContent = interpretValue + "; " + priceValue;
+        display.appendChild(newElement);
+    }
 }
+
